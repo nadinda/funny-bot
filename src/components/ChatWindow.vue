47acc278f50e6c045ef0da23878a272e25ba07dd
Nadinda <nadinda@nadinda.dev>
2023-04-10 19:27:01 +0200

    Store user input and show them in message bubble

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,24 +1,57 @@
 <template>
   <div class="chat-container">
     <div class="chat-messages">
-      <div class="message-bot">
-        <div class="message-text">Welcome! I'm Funny Bot</div>
-        <div class="message-sender">bot</div>
-      </div>
-      <div class="message-user">
-        <div class="message-text">Hello, I'm user</div>
-        <div class="message-sender">user</div>
+      <div
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="{
+          'message-bot': message.sender === 'bot',
+          'message-user': message.sender === 'user',
+        }"
+      >
+        <div class="message-text">{{ message.content }}</div>
+        <div class="message-sender">{{ message.sender }}</div>
       </div>
     </div>
     <div class="chat-input">
-      <form>
-        <input type="text" placeholder="Type your message..." />
+      <form @submit.prevent="sendMessage">
+        <input
+          v-model="userInput"
+          type="text"
+          placeholder="Type your message..."
+        />
       </form>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Message {
+  content: string;
+  sender: "user" | "bot";
+}
+
+const messages = ref<Message[]>([]);
+const userInput = ref("");
+
+const sendMessage = () => {
+  if (userInput.value.trim() !== "") {
+    messages.value.push({
+      content: userInput.value,
+      sender: "user",
+    });
+
+    messages.value.push({
+      content: "hi",
+      sender: "bot",
+    });
+
+    userInput.value = "";
+  }
+};
+</script>
 
 <style scoped>
 .chat-container {
